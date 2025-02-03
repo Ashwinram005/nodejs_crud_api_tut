c3d4738b2c73e083c44d2a572b463491e6d0f907
Ashwinram005 <ashwinrammohan2005@gmail.com>
2025-02-03 21:42:19 +0530

    updated

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5001;
 
 const server = http.createServer((req, res) => {
+    res.statusCode=200
+    res.setHeader('Content-Type', 'application/json');
+    res.write(JSON.stringify({message: 'ASHWINRAM'}));
+    res.end();
+})
+
+server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
